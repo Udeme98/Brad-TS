a35e0fd25264516f9ffe3db0aa88d5435e650dd9
Udeme98 <Darealudeme1@gmail.com>
2024-07-21 22:21:50 +0100

    Brad-TS

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+// console.log("Hello TypeScript");
+// Basic Types
+let num = 1; //Number type
+let firstName = "Udeme"; //String type
+let isAvailable = true; //Boolean type
+let x = 2; //Any type
+//Arrays
+let nums = [1, 2, 3, 4, 5];
+let randomArray = [1, "Udeme", true];
+//Tuples
+let person = [1, "Joe", true];
+//Tuples Array
+let employee;
+employee = [
+    [1, "James"],
+    [2, "Sam"],
+    [3, "Pete"],
+];
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// console.log("Hello TypeScript");
+
+// Basic Types
+let num: number = 1; //Number type
+let firstName: string = "Udeme"; //String type
+let isAvailable: boolean = true; //Boolean type
+let x: any = 2; //Any type
+
+//Arrays
+let nums: number[] = [1, 2, 3, 4, 5];
+let randomArray: any[] = [1, "Udeme", true];
+
+//Tuples
+let person: [number, string, boolean] = [1, "Joe", true];
+
+//Tuples Array
+let employee: [number, string][];
+employee = [
+  [1, "James"],
+  [2, "Sam"],
+  [3, "Pete"],
+];
